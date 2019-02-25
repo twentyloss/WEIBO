94216a23b5447b0e5bbe6adf569103364c69f321
lxy <1252153341@qq.com>
2019-02-25 15:34:00 +0800

    user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,3 +6,32 @@
  * Time: 14:57
  */
 namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $table = 'users';
+
+}
