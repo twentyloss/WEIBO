b29dcf4484f4814353ea000e43664b6c63a14f7c
lxy <1252153341@qq.com>
2019-02-25 17:03:47 +0800

    chinese message for noticing the error

---
+++ resources/lang/zh-CN/validation.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lxy
+ * Date: 2019/2/25
+ * Time: 16:55
+ */
+return[
+'custom' => [
+    'email' => [
+        'required' => '邮箱要写的呀！',
+    ],
+],
+    ];
